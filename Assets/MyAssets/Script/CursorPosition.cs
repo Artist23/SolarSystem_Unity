63e4e8170a1227b8d8c876a7d15392476f439eb6
Yongqing1995 <936498484@qq.com>
2018-05-05 20:12:55 +0300

    Full version

---
+++ Assets/MyAssets/Script/CursorPosition.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CursorPosition : MonoBehaviour {
+
+    public Camera cam;
+    private Vector3 originalScale;         //the original scale of the cursor
+	void Start () {
+        originalScale = this.transform.localScale;   //get the original scale
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        float distance;           //distance of the cursor 
+        RaycastHit hit;
+        Ray ray = new Ray(cam.transform.position, 
+            cam.transform.rotation * Vector3.forward);    //set the ray
+        if (Physics.Raycast(ray, out hit))
+        {
+            distance = hit.distance*0.9f;           //if something was hit, then let cursor be there
+        }
+        else
+        {
+            distance = cam.farClipPlane * 0.95f;    //else let cursor stick on the far clip plane
+        }
+
+        this.transform.position = cam.transform.position +
+           cam.transform.rotation * Vector3.forward * distance;  //set the transform of cursor use distance
+
+        this.transform.localScale = originalScale * distance;    //change the scale
+    }
+}
