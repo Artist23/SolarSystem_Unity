a181aba4ad7585621e3153bb6717a8b8530df804
Yongqing1995 <936498484@qq.com>
2018-05-10 21:45:13 +0300

    没什么问题

--- Assets/MyAssets/Script/Judging.cs
+++ Assets/MyAssets/Script/Judging.cs
@@ -6,31 +6,31 @@ using System.Xml;
 
 public class Judging : MonoBehaviour  {
 
-    string rightAnswer;
-    string myAns;
-    GameObject scoreBoard;
-    GameObject button1,button2,button3;
-    Text score;
-    float countDown=2.0f;
-    public bool updateOn = true;
+    string rightAnswer;    //the right answer
+    string myAns;          //the answer that user chose
+    GameObject scoreBoard; //the score board 
+    GameObject button1,button2,button3;   //3 button for options
+    Text score;                           //score
+    float countDown=2.0f;                 //focus on a button for 2 seconds to select it
+    public bool updateOn = true;          //update control
     float lastTime;
 
 
     void Start () {
         
-        rightAnswer = ShowQuestion.GetRightAnswer();
-        scoreBoard = GameObject.Find("ScoreBoard");
-        score=scoreBoard.GetComponentInChildren<Text>();
-        button1 = GameObject.Find("Button1");
+        rightAnswer = ShowQuestion.GetRightAnswer();      //get the right answer
+        scoreBoard = GameObject.Find("ScoreBoard");       //get scoreboard object
+        score=scoreBoard.GetComponentInChildren<Text>();  //get text componnet of scoreboard
+        button1 = GameObject.Find("Button1");             //get all the buttons with names
         button2 = GameObject.Find("Button2");
         button3 = GameObject.Find("Button3");
-        button1.GetComponent<Button>().onClick.AddListener(() => Judge(button1));
+        button1.GetComponent<Button>().onClick.AddListener(() => Judge(button1));  //listen to onClick event, then call Judge()
         button2.GetComponent<Button>().onClick.AddListener(() => Judge(button2));
         button3.GetComponent<Button>().onClick.AddListener(() => Judge(button3));
              
     }
 
-    void Judge(GameObject button)
+    void Judge(GameObject button)   //judge the button that user have just selected
     {
         if (Time.time - lastTime < 5)
         {
@@ -40,17 +40,19 @@ public class Judging : MonoBehaviour  {
         {
             rightAnswer = ShowQuestion.GetRightAnswer();
         }
-        myAns = button.GetComponentInChildren<Text>().text;
-        if(myAns==rightAnswer)
+
+        myAns = button.GetComponentInChildren<Text>().text;  //get text on the selected button
+
+        if(myAns==rightAnswer)  //if the answer is right
         {
             score.text = "That's right!";
-            ShowQuestion.ReadTask();
-            ShowQuestion.Show();
-            rightAnswer = ShowQuestion.GetRightAnswer();
+            ShowQuestion.ReadTask();       //read the next question and options and right answer
+            ShowQuestion.Show();           //show the next question and options
+            rightAnswer = ShowQuestion.GetRightAnswer();  //update the right answer 
         }
         else
         {
-            score.text = "Try Again";
+            score.text = "Try Again";  //if not right then print try again
         }
         lastTime = Time.time;
         updateOn = true;
@@ -58,9 +60,9 @@ public class Judging : MonoBehaviour  {
 
 	void Update () {
 
-        if (updateOn == true)
+        if (updateOn == true)  
         {
-            updateOn = false;
+            updateOn = false;  //update pause
             
         }  
     }
--- Assets/MyAssets/Script/ShowQuestion.cs
+++ Assets/MyAssets/Script/ShowQuestion.cs
@@ -6,57 +6,58 @@ using UnityEngine.UI;
 
 public class ShowQuestion : MonoBehaviour {
 
-    static int nQuestionNumber=1;
-    static string strQuestionPath;
-    static Text questionSheet;
-    static string strQuestion;
-    static string strRightAnswer;
-    static string a1, a2, a3;
+    static int nQuestionNumber=1;    //question index
+    static string strQuestionPath;   //question‘s path
+    static Text questionSheet;       //the text on UI which is called question sheet
+    static string strQuestion;       //Question 
+    static string strRightAnswer;    //the right answer
+    static string a1, a2, a3;        //3 options
 
-    static string GetNextQuestionPath ()
+    static string GetNextQuestionPath ()  //get question path
     {
-         strQuestionPath = string.Format("QA/Question{0}", nQuestionNumber);
-         ++nQuestionNumber; 
-         return strQuestionPath;
+         strQuestionPath = string.Format("QA/Question{0}", nQuestionNumber);  //set question path with its number
+         ++nQuestionNumber;          //ready to get next question
+         return strQuestionPath;     //return the question path
     }
 
-    public static void ReadTask()
+    public static void ReadTask()    //read the whole task(question + 3 options)
     {
         if (nQuestionNumber > ReadFromXml.NumberOfQuestions("\\MyAssets\\Resources\\QA.xml"))
         {
-            return;
+            return;    //to end the tasks
         }
-        XmlNode node = ReadFromXml.Read("\\MyAssets\\Resources\\QA.xml", GetNextQuestionPath());
-        strQuestion = node.SelectSingleNode("q").InnerText;
-        a1 = node.SelectSingleNode("a1").InnerText;
+        //get the target node with question path
+        XmlNode node = ReadFromXml.Read("\\MyAssets\\Resources\\QA.xml", GetNextQuestionPath()); 
+        strQuestion = node.SelectSingleNode("q").InnerText;   //get question string
+        a1 = node.SelectSingleNode("a1").InnerText;           //get all 3 options
         a2 = node.SelectSingleNode("a2").InnerText;
         a3 = node.SelectSingleNode("a3").InnerText;
-        strRightAnswer = node.SelectSingleNode("rightAnswer").InnerText;
+        strRightAnswer = node.SelectSingleNode("rightAnswer").InnerText;  //get the right answer
     }
 
-    public static void Show()
+    public static void Show()     //add to UI
     {
-        questionSheet.text = strQuestion;
-        GameObject[] buttons = GameObject.FindGameObjectsWithTag("Button");
-        buttons[0].GetComponentInChildren<Text>().text = a1;
+        questionSheet.text = strQuestion;     //show question
+        GameObject[] buttons = GameObject.FindGameObjectsWithTag("Button"); //get all the 3 button
+        buttons[0].GetComponentInChildren<Text>().text = a1;  //change the button text to options
         buttons[1].GetComponentInChildren<Text>().text = a2;
         buttons[2].GetComponentInChildren<Text>().text = a3;
     }
 
-    public static string GetRightAnswer()
+    public static string GetRightAnswer()  //get the right answer
     {
         return strRightAnswer;
     }
 
 	void Start () {
        
-        TextAsset myText = Resources.Load("QA") as TextAsset;
-        ReadTask();
-        questionSheet = gameObject.GetComponent<Text>();
+        TextAsset myText = Resources.Load("QA") as TextAsset;  //load Xml file from resources
+        ReadTask();                                            //read the whole task
+        questionSheet = gameObject.GetComponent<Text>();       //get the text component of UI
 
         if (nQuestionNumber <= ReadFromXml.NumberOfQuestions("\\MyAssets\\Resources\\QA.xml"))
         {
-            Show();
+            Show();   
         }
 
     }
